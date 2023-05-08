cb071429005ac5327c30d44b2da68076df43f596
vh <vhernest@gmail.com>
2023-05-08 13:15:33 +0300

    hello world

---
+++ app/src/main/java/com/course/evremea/MainActivity.java
@@ -0,0 +1,52 @@
+package com.course.evremea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHello;
+    private Button buttonGo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvHello = findViewById(R.id.tv_hello);
+        buttonGo = findViewById(R.id.button_go);
+
+        buttonGo.setOnClickListener(view -> {
+            tvHello.setText("Hello");
+
+            new Thread(() -> {
+                try {
+                    URLConnection conn = new URL("http://172.19.171.153:4567/api/v1/weather/cities/city/Baia%20Mare").openConnection();
+
+                    byte[] buffer = new byte[256];
+                    int count = conn.getInputStream().read(buffer);
+                    String response = new String(buffer, 0, count);
+                    JSONObject jsonObject = new JSONObject(response);
+                    int temperature = jsonObject.getInt("temperature");
+
+                    runOnUiThread(() -> {
+                        tvHello.setText("" + temperature);
+                    });
+                } catch(IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        });
+    }
+}
